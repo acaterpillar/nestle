8dc504931be573f29eeaedb67f79e0f1727df315
ronger <ronger-x@outlook.com>
2021-12-13 09:29:19 +0800

    Initial commit

---
+++ src/test/java/com/rymcu/nestle/NestleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rymcu.nestle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NestleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
